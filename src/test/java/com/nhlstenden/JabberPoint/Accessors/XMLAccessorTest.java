1c1a8953b9c19452c773096e8b5dc56fb8cd4932
jariolyslager <jari.olijslager@student.nhlstenden.com>
2024-04-07 13:18:41 +0200

    Unit tests for XMLAccessor

---
+++ src/test/java/com/nhlstenden/JabberPoint/Accessors/XMLAccessorTest.java
@@ -0,0 +1,57 @@
+package com.nhlstenden.JabberPoint.Accessors;
+
+import com.nhlstenden.JabberPoint.PresentationControls.Presentation;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLAccessorTest
+{
+    private static ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private static PrintStream originalErr = System.err;
+
+    @BeforeAll
+    static void init()
+    {
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterAll
+    static void restore()
+    {
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void loadFile_existingFile_expectSuccess() throws Exception
+    {
+        XMLAccessor xmlAccessor = new XMLAccessor();
+        Presentation presentation = new Presentation();
+
+        xmlAccessor.loadFile(presentation, "test.xml");
+
+        assertEquals("XML-Based Presentation for Jabberpoint", presentation.getTitle());
+        assertEquals(5, presentation.getSize());
+        assertEquals("JabberPoint XML-Demo", presentation.getShowList().get(0).getTitle());
+        assertEquals(9, presentation.getShowList().get(0).getItems().size());
+        assertEquals("TextItem[1, Welkom bij Jabberpoint]", presentation.getShowList().get(0).getItems().get(0).toString());
+    }
+
+    @Test
+    void loadFile_nonExistingFile_expectException() throws IOException
+    {
+
+        XMLAccessor xmlAccessor = new XMLAccessor();
+        Presentation presentation = new Presentation();
+        xmlAccessor.loadFile(presentation, "file.xml");
+
+
+        assertTrue(errContent.toString().contains("File not found in resources"));
+    }
+}
